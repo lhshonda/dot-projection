e807e6e73ef31407b6fff758d304f4794de60be7
lucas honda <103671864+lhshonda@users.noreply.github.com>
2025-11-08 05:42:41 -0500

    feat: create camera feed

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CameraTest from './CameraTest';
+// import FaceDots from './FaceDots';  // We'll use this later
+
+function App() {
+  return <CameraTest />;
+}
+
+export default App;
